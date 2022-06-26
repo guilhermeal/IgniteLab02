5452361933ab17cac6cde4b1af2fd67e0e7d3e23
Guilherme Almeida <guiallan.al@gmail.com>
2022-06-26 05:45:18 -0300

    Class 4 - Signup subscribers

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4p6d9vq1sc401xl3twq7v3t/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
